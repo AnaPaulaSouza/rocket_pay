cdd1cdcc49e4ee4bfd15e833037937447131f536
Ana Paula Souza <ana.pdsouza@b2wdigital.com>
2021-02-24 14:16:27 -0300

    nlw4: aula 1

---
+++ config/config.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+config :rocketpay,
+  ecto_repos: [Rocketpay.Repo]
+
+# Configures the endpoint
+config :rocketpay, RocketpayWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "jJn0lYzzVZfTREN3s2Wlu1bEnWRZs96xox5je5CldOl/an/HLIRLcjYuvTyz21yD",
+  render_errors: [view: RocketpayWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Rocketpay.PubSub,
+  live_view: [signing_salt: "0YCN5B8k"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+import_config "#{Mix.env()}.exs"
---
+++ config/dev.exs
@@ -0,0 +1,27 @@
+use Mix.Config
+
+# Configure your database
+config :rocketpay, Rocketpay.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "rocketpay_dev",
+  hostname: "localhost",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
+config :rocketpay, RocketpayWeb.Endpoint,
+  http: [port: 8080],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: []
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
+
+# Initialize plugs at runtime for faster development compilation
+config :phoenix, :plug_init_mode, :runtime
